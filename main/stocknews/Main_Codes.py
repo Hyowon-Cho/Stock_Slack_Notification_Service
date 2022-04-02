222721edd6304969a5210fc69f6ed72467c8daec
Lukas Cho <63900561+Hyowon-Cho@users.noreply.github.com>
2022-04-01 23:16:23 -0400

    Update Main_Codes.py

--- main/stocknews/Main_Codes.py
+++ main/stocknews/Main_Codes.py
@@ -9,14 +9,14 @@ app = Flask("MadScrapper")
 @app.route("/")
 def home():
     url1 = "https://finance.yahoo.com/"
-    result_naver = requests.get(url1)
-    soup_naver = BeautifulSoup(result_naver.text, "html.parser")
+    result_yahoo = requests.get(url1)
+    soup_yahoo = BeautifulSoup(result_yahoo.text, "html.parser")
 
     url2 = "https://news.google.com/search?q=stock&hl=en-US&gl=US&ceid=US%3Aen"
     result_google = requests.get(url2)
     soup_google = BeautifulSoup(result_google.text, "html.parser")
 
-    dds_n = soup_naver.find_all('dd', {"class": "articleSubject"})
+    dds_n = soup_yahoo.find_all('dd', {"class": "articleSubject"})
     as_g = soup_google.find_all('h3', {"class": "RD0gLb"})
     # print(as_g)
 
@@ -26,7 +26,7 @@ def home():
     for dd in dds_n:
         title = dd.find('a').text
         link = dd.find('a')['href']
-        link = "https://finance.naver.com/"+link
+        link = "https://finance.yahoo.com/"+link
         article_n.append({'Title': title, 'Link': link})
     print(article_n)
 
